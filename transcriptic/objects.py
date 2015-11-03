483fecad94a8b3d9ce12a65bd4c5f85710fd80d0
Tali Herzka <tali@herzka.com>
2015-11-03 15:11:42 -0800

    fix indentation in objects

--- transcriptic/objects.py
+++ transcriptic/objects.py
@@ -20,36 +20,44 @@ class ProtocolPreview(object):
       self.preview_url
 
 class Instructions(object):
-  '''
-  An instruction object contains raw instructions as JSON as well as list of
-  operations and warps generated from the raw instructions.
+        '''
+        An instruction object contains raw instructions as JSON as well as list of
+        operations and warps generated from the raw instructions.
 
-  Parameters
-  ----------
+        Parameters
+        ----------
+        raw_instructions : dict
+           Dictionary of raw instructions.
 
-  raw_instructions : dict
-    raw instruction dictionary
+        Attributes
+        ----------
+        raw_instructions : dict
+            Dictionary of raw instructions.
+        df : DataFrame
+            Pandas DataFrame composed of each instruction's name and its
+            associated list of warps.
 
-  '''
-  def __init__(self, raw_instructions):
-    self.raw_instructions = raw_instructions
-    op_name_list = []
-    op_warp_list = []
-    for instruction in raw_instructions:
-      op_name_list.append(instruction["operation"]["op"])
-      op_warp_list.append(instruction["warps"])
-    instruct_dict = {}
-    instruct_dict["name"] = op_name_list
-    instruct_dict["warp_list"] = op_warp_list
-    self.df = pandas.DataFrame(instruct_dict)
+       '''
+      def __init__(self, raw_instructions):
+        self.raw_instructions = raw_instructions
+        op_name_list = []
+        op_warp_list = []
+        for instruction in raw_instructions:
+          op_name_list.append(instruction["operation"]["op"])
+          op_warp_list.append(instruction["warps"])
+        instruct_dict = {}
+        instruct_dict["name"] = op_name_list
+        instruct_dict["warp_list"] = op_warp_list
+        self.df = pandas.DataFrame(instruct_dict)
 
 class Dataset(object):
-  def __init__(self, id, attributes, connection = False):
-    super(Dataset, self).__init__()
-    self.id = id
-    self.attributes = attributes
-    self.connection = connection
-    # self.df = pandas.DataFrame(attributes)
+
+    def __init__(self, id, attributes, connection = False):
+      super(Dataset, self).__init__()
+      self.id = id
+      self.attributes = attributes
+      self.connection = connection
+      # self.df = pandas.DataFrame(attributes)
 
   def _repr_html_(self):
     return """<iframe src="%s" frameborder="0" allowtransparency="true" style="height:500px;" seamless></iframe>""" % \
@@ -113,12 +121,28 @@ class Project(object):
     req_json = api_submit(protocol, self.id, title, test_mode)
     return Run(req_json['id'], req_json)
 
+  def data(self):
+    req = self.connection.get("%s/runs/%s/data" % (
+      self.attributes['project']['url'],
+      self.id
+    ))
+    if req.status_code == 200:
+      response = req.json()
+      return {k: Dataset(k, v) for k, v in response.iteritems()}
+    elif req.status_code == 404:
+      raise Exception("[404] No run found for ID " + id)
+    else:
+      raise Exception("[%d] %s" % (req.status_code, req.json()))
+
+    def _repr_html_(self):
+      return Render(self)._repr_html_()
+
 class Aliquot(object):
-  def __init__(self, id, attributes, connection = False):
-    super(Aliquot, self).__init__()
-    self.id = id
-    self.attributes = attributes
-    self.connection = connection
+    def __init__(self, id, attributes, connection = False):
+      super(Aliquot, self).__init__()
+      self.id = id
+      self.attributes = attributes
+      self.connection = connection
 
 
 class Container(object):
@@ -186,8 +210,8 @@ class Container(object):
 
 
 class Resource(object):
-  def __init__(self, id, attributes, connection = False):
-    super(Resource, self).__init__()
-    self.id = id
-    self.attributes = attributes
-    self.connection = connection
+    def __init__(self, id, attributes, connection = False):
+      super(Resource, self).__init__()
+      self.id = id
+      self.attributes = attributes
+      self.connection = connection
