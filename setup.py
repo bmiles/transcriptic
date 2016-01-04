e8202a34b9c9ea1c78d10ad7d386487d42f3eff5
Tali Herzka <tali@herzka.com>
2016-01-04 12:46:19 -0800

    see if removing requirements works

--- setup.py
+++ setup.py
@@ -8,15 +8,7 @@ setup(
     packages=['transcriptic', 'transcriptic.analysis'],
     install_requires=[
         'Click>=5.1',
-        'requests',
-        'autoprotocol>=2.5',
-        'pandas>=0.16',
-        'matplotlib>=1.4',
-        'scikit-learn>=0.16',
-        'scipy>=0.16',
-        'numpy>=1.10',
-        'plotly>=1.8',
-        'future>=0.15'
+        'requests'
     ],
     entry_points='''
         [console_scripts]
